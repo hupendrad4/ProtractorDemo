b12329facab6c80b66f971907a7a34ed9d4fd1b5
Hupendra Chudaman Deore <HDeore@slb.com>
2021-05-04 20:40:12 +0530

    [new files]

--- conf/conf.js
+++ conf/conf.js
@@ -1,5 +1,5 @@
 // An example configuration file.
-var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+var HtmlScreenshotReporter = require('C:/Users/HDeore/node_modules/protractor-jasmine2-screenshot-reporter');
 
 var reporter = new HtmlScreenshotReporter({
   dest: 'target/screenshots',
@@ -21,28 +21,67 @@ exports.config = {
   // Spec patterns are relative to the current working directory when
   // protractor is called.
   specs: ['../tests/calculator.js'],
- 
+
   // Options to be passed to Jasmine.
-  jasmineNodeOpts:{
-    defaultTimeoutInterval:3000
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 3000
   },
-    // Setup the report before any tests start
-    beforeLaunch: function() {
-      return new Promise(function(resolve){
-        reporter.beforeLaunch(resolve);
-      });
-    },
-  
-    // Assign the test reporter to each running instance
-    onPrepare: function() {
-      jasmine.getEnv().addReporter(reporter);
-    },
-  
-    // Close the report after all tests finish
-    afterLaunch: function(exitCode) {
-      return new Promise(function(resolve){
-        reporter.afterLaunch(resolve.bind(this, exitCode));
-      });
-    }
-  
+  // Setup the report before any tests start
+  beforeLaunch: function () {
+    return new Promise(function (resolve) {
+      reporter.beforeLaunch(resolve);
+    });
+  },
+
+  // Assign the test reporter to each running instance
+  onPrepare: function () {
+
+    var jasmineReporters = require('C:/Users/HDeore/node_modules/jasmine-reporters');
+    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+      consolidateAll: true,
+      savePath: './',
+      filePrefix: 'xmlresults'
+    }));
+
+    jasmine.getEnv().addReporter(reporter);
+    var AllureReporter = require('C:/Users/HDeore/node_modules/jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter({
+      resultsDir: 'allure-results'
+    }));
+  },
+
+  // Close the report after all tests finish
+  afterLaunch: function (exitCode) {
+    return new Promise(function (resolve) {
+      reporter.afterLaunch(resolve.bind(this, exitCode));
+    });
+  },
+
+  //HTMLReport called once tests are finished
+  onComplete: function () {
+    var browserName, browserVersion;
+    var capsPromise = browser.getCapabilities();
+
+    capsPromise.then(function (caps) {
+      browserName = caps.get('browserName');
+      browserVersion = caps.get('version');
+      platform = caps.get('platform');
+
+      var HTMLReport = require('C:/Users/HDeore/node_modules/rotractor-html-reporter-2');
+
+      testConfig = {
+        reportTitle: 'Protractor Test Execution Report',
+        outputPath: './',
+        outputFilename: 'ProtractorTestReport',
+        screenshotPath: './screenshots',
+        testBrowser: browserName,
+        browserVersion: browserVersion,
+        modifiedSuiteName: false,
+        screenshotsOnlyOnFailure: true,
+        testPlatform: platform
+      };
+      new HTMLReport().from('xmlresults.xml', testConfig);
+    });
+  }
+
 };
--- tests/calculator.js
+++ tests/calculator.js
@@ -1,6 +1,16 @@
 let homepage = require('../pages/homepage');
 
 describe('Test Calculator app', function () {
+    var originalTimeout;
+
+    beforeEach(function() {
+        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000000;
+    });
+
+    afterEach(function() {
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    });
 
     it('addition test', function () {
 
@@ -15,9 +25,54 @@ describe('Test Calculator app', function () {
         homepage.verifyResult(12);
 
         browser.sleep(10000)
-    })
+    });
+    it('Substraction test', function () {
+
+        homepage.getURL('http://juliemr.github.io/protractor-demo/');
+
+        homepage.enterFirstNumber(5);
+
+        homepage.enterSecondNumber(8);
+
+        homepage.clickGoButton();
+
+        homepage.verifyResult(12);
+
+        browser.sleep(10000)
+    });
+
+    it('Division test', function () {
+
+        homepage.getURL('http://juliemr.github.io/protractor-demo/');
+
+        homepage.enterFirstNumber(5);
+
+        homepage.enterSecondNumber(8);
+
+        homepage.clickGoButton();
+
+        homepage.verifyResult(12);
+
+        browser.sleep(10000)
+    });
+
+    it('Multiplication test', function () {
+
+        homepage.getURL('http://juliemr.github.io/protractor-demo/');
+
+        homepage.enterFirstNumber(5);
+
+        homepage.enterSecondNumber(8);
+
+        homepage.clickGoButton();
+
+        homepage.verifyResult(12);
+
+        browser.sleep(10000)
+    });
 
 
 
 
-})
+
+});
